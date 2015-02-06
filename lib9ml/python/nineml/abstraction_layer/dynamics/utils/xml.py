russellijarvis/nineml

Copied additions from constants branch and renamed constant to randomvariable

lib9ml/python/nineml/abstraction_layer/dynamics/utils/xml.py
@@ -79,12 +79,13 @@ class DynamicsClassXMLLoader(ComponentClassXMLLoader):
 
     @read_annotations
     def load_dynamics(self, element):
-        subblocks = ('Regime', 'Alias', 'StateVariable')
+        subblocks = ('Regime', 'Alias', 'StateVariable', 'RandomVariable')
         subnodes = self._load_blocks(element, blocks=subblocks)
 
         return Dynamics(regimes=subnodes["Regime"],
                         aliases=subnodes["Alias"],
-                        state_variables=subnodes["StateVariable"])
+                        state_variables=subnodes["StateVariable"],
+                        randomvariables=subnodes["RandomVariable"])
 
     @read_annotations
     def load_regime(self, element):
@@ -183,6 +184,7 @@ class DynamicsClassXMLWriter(ComponentClassXMLWriter):
     def visit_dynamics(self, dynamics):
         elements = ([r.accept_visitor(self) for r in dynamics.regimes] +
                     [b.accept_visitor(self) for b in dynamics.aliases] +
+                    [c.accept_visitor(self) for c in dynamics.randomvariables] +
                     [b.accept_visitor(self) for b in dynamics.state_variables])
         return E('Dynamics', *elements)
 

INCF/old_nineml_repo

lib9ml/python/nineml/abstraction_layer/dynamics/utils/xml.py
@@ -79,12 +79,13 @@ class DynamicsClassXMLLoader(ComponentClassXMLLoader):
 
     @read_annotations
     def load_dynamicsblock(self, element):
-        subblocks = ('Regime', 'Alias', 'StateVariable')
+        subblocks = ('Regime', 'Alias', 'StateVariable', 'RandomVariable')
         subnodes = self._load_blocks(element, blocks=subblocks)
 
         return DynamicsBlock(regimes=subnodes["Regime"],
                         aliases=subnodes["Alias"],
-                        state_variables=subnodes["StateVariable"])
+                        state_variables=subnodes["StateVariable"],
+                        randomvariables=subnodes["RandomVariable"])
 
     @read_annotations
     def load_regime(self, element):
@@ -184,7 +185,8 @@ class DynamicsClassXMLWriter(ComponentClassXMLWriter):
         elements = ([b.accept_visitor(self)
                      for b in dynamicsblock.state_variables] +
                     [r.accept_visitor(self) for r in dynamicsblock.regimes] +
-                    [b.accept_visitor(self) for b in dynamicsblock.aliases])
+                    [b.accept_visitor(self) for b in dynamicsblock.aliases] +
+                    [c.accept_visitor(self) for c in dynamics.randomvariables])
         return E('Dynamics', *elements)
 
     @annotate_xml